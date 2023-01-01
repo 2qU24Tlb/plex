1d5a3121721315722c0245a71daf3f2b3ab66e5c
Hao <unioah@gmail.com>
2023-01-01 13:33:34 -0500

    update plex

--- plex/boot.sh
+++ plex/boot.sh
@@ -7,7 +7,7 @@ export PLEX_MEDIA_SERVER_INFO_PLATFORM_VERSION=$(uname -r)
 rm -rf /data/config/Library/Application\ Support/Plex\ Media\ Server/plexmediaserver.pid
 mkdir -p /root/Library/TV /root/Library/Movie
 
-rclone copy my:/mount.sh .
+rclone copy myone:/mount.sh .
 
 bash mount.sh
 
